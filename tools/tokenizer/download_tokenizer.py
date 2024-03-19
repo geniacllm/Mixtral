2935c5a1aa7473142326b38667c8025c6318c5cc
kumagai6 <kumagai@star.rcast.u-tokyo.ac.jp>
2024-03-20 05:44:08 +0900

    UPDATE:tokenizer download

--- tools/tokenizer/download_tokenizer.py
+++ tools/tokenizer/download_tokenizer.py
@@ -1,4 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
 
+home_directory = os.getenv('HOME')
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
-tokenizer.save_pretrained("./tokenizer_model_directory")
+tokenizer.save_pretrained(home_directory+"/moe-recipes/tokenizer_model_directory")
