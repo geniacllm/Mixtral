e6e4420b7dac4c147d7dc5064419ef9e530aefa7
Eric <zhaoyichong@outlook.com>
2024-04-10 06:56:05 +0900

    Modify: download_token.py

--- tools/tokenizer/download_tokenizer.py
+++ tools/tokenizer/download_tokenizer.py
@@ -1,6 +1,10 @@
+import sys
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import os
 
+# read path from std input
+path = sys.argv[1]
+
 home_directory = os.getenv('HOME')
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
-tokenizer.save_pretrained("/mnt/nfs-mnj-home-43/i23_eric/code-server/userdata/Mixtral/tokenizer_model_directory")
+tokenizer.save_pretrained(path)
