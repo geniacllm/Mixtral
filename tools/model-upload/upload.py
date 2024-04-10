d506a9d18b5fc7eb150adc802f898adf81a1d362
Eric <zhaoyichong@outlook.com>
2024-04-10 17:53:33 +0900

    Update

--- tools/model-upload/upload.py
+++ tools/model-upload/upload.py
@@ -19,28 +19,9 @@ except Exception as e:
     print(f"repo {repo_name} already exists! error: {e}")
     pass
 
-files = os.listdir(converted_ckpt)
-print(f"files: {files}")
-
-def upload_directory(directory_path, repo_name, branch_name, repo_type="model"):
-    api = HfApi()
-
-    for root, dirs, files in os.walk(directory_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            path_in_repo = os.path.relpath(file_path, start=directory_path)
-            print(f"Uploading {file_path} to {path_in_repo}...")
-
-            api.upload_file(
-                path_or_fileobj=file_path,
-                path_in_repo=path_in_repo,
-                repo_id=repo_name,
-                repo_type=repo_type,
-                commit_message=f"Upload {path_in_repo}",
-                revision=branch_name,
-            )
-            print(f"Successfully uploaded {path_in_repo} !")
-
+files_and_dirs = os.listdir(converted_ckpt)
+files = [f for f in files_and_dirs if os.path.isfile(os.path.join(converted_ckpt, f))]
+print(f"Files to upload: {files}")
 
 api = HfApi()
 if branch_name != "main":
@@ -52,16 +33,21 @@ if branch_name != "main":
         )
     except Exception:
         print(f"branch {branch_name} already exists, try again...")
-        
+
 print(f"to upload: {files}")
 for file in files:
-    print(f"Uploading {file} to branch {branch_name}...")
-    api.upload_file(
-        path_or_fileobj=os.path.join(converted_ckpt, file),
-        path_in_repo=file,
-        repo_id=repo_name,
-        repo_type="model",
-        commit_message=f"Upload {file}",
-        revision=branch_name,
-    )
-    print(f"Successfully uploaded {file} !")
+    file_path = os.path.join(converted_ckpt, file)
+    if os.path.isfile(file_path): # check if it's a file
+        print(f"Uploading {file} to branch {branch_name}...")
+        try:
+            api.upload_file(
+                path_or_fileobj=file_path,
+                path_in_repo=file,
+                repo_id=repo_name,
+                repo_type="model",
+                commit_message=f"Upload {file}",
+                revision=branch_name,
+            )
+            print(f"Successfully uploaded {file} !")
+        except Exception as e:
+            print(f"Failed to upload {file}: {e}")
